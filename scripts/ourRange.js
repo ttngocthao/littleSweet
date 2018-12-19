d48edb8d66f0bfde754794e8c160e4d37ce4206d
Thao Thi Ngoc Truong <ttngocthao_87@yahoo.com>
2018-12-19 09:22:15 +0000

    addBtn works,done showing how many item in basket.

--- scripts/ourRange.js
+++ scripts/ourRange.js
@@ -31,8 +31,12 @@ xhr.onload = function(){
                 <h2>${choice.options[choice.selectedIndex].text}</h2>
                 ${cakeArray.map(cakeTemplate).join('')}   
             `
-            //render the information box
+            //add the click event to each Info button
             clickInfoBtn();
+
+            addToCartBtn();
+
+            
            
         })
     }
@@ -47,7 +51,8 @@ function cakeTemplate(cakeChoice){
     return  `<div class='item'>
                 <h3>${cakeChoice.name}</h3>
                 <h5>${cakeChoice.price}</h5>
-                <button class='infoBtn' id='infoBtn${cakeChoice.itemId}'>More Info</button>              
+                <button class='infoBtn' id='infoBtn${cakeChoice.itemId}'>More Info</button>
+                <button class='addToCartBtn' dataId='${cakeChoice.itemId}' dataName='${cakeChoice.name}'  dataPrice='${cakeChoice.price}'>Add to basket</button>              
                 <div id='infoBox${cakeChoice.itemId}' class='infoBox'>
                     <div class='infoContent'>
                         <span class="closeBtn" id='closeBtn${cakeChoice.itemId}'>&times;</span>
@@ -59,16 +64,36 @@ function cakeTemplate(cakeChoice){
             </div> `
 }
 //btn has id= infoBtn.... (itemId) and infomation box has id= inforBox....(itemId)
-//figure out which infoBtn is clicked
+//this function add a click event to a InfoBtn which is clicked by users
 function clickInfoBtn(){
     var infoBtns = document.getElementsByClassName("infoBtn");
-    
+    //figure out which infoBtn is clicked
     //add click event to the Info btn which is clicked        
     for (var i = 0; i <infoBtns.length; i++) {
         var infoBtn=infoBtns[i];
         infoBtn.addEventListener('click', renderInfoBox);
     }    
 }
+
+//this function add a click event to a addToCart Btn
+function addToCartBtn(){
+    var btns = document.getElementsByClassName('addToCartBtn');
+    //loop through the addToCartBtn to add a click event to each btn
+    for (var i = 0; i < btns.length; i++){
+        btns[i].addEventListener('click', function(){
+            var idNo= this.getAttribute('dataId');
+            var name= this.getAttribute('dataName');
+            var price= this.getAttribute('dataPrice');
+            addItemToCart(idNo,name,price,1);
+
+            //when the item is added, show how many items in basket
+            document.getElementById('cartIcon').innerHTML=countCart();
+        })
+    }
+    
+}
+
+
 //show the information of the product
 function renderInfoBox(){  
         var idBtn, idBox, idClose;  
@@ -88,8 +113,48 @@ function renderInfoBox(){
 }
 
 //search Cake
-var searchBar = document.forms['searchCake'].querySelector('input');
-searchBar.addEventListener('keyup',function(e){
-    var term = e.target.value.toLowerCase();
-    var items = document.getElementsByClassName('item');
-})
+// var searchBar = document.forms['searchCake'].querySelector('input');
+// searchBar.addEventListener('keyup',function(e){
+//     var term = e.target.value.toLowerCase();
+//     var items = document.getElementsByClassName('item');
+// })
+
+
+
+//object constructor
+var Item = function(id, name, price, count){
+    this.id= id;
+    this.name = name;
+    this.price = price;
+    this.count = count;
+}
+
+//variable to hold the items in cart.
+var cart=[];
+
+//function add Item to cart
+function addItemToCart(id, name, price, count){
+    //if there is the same product in the basket,
+    //increase the count.
+    for(var i in cart){
+        if(cart[i].id === id){
+            cart[i].count +=count;
+            return;            
+        }
+    }
+    var item = new Item(id, name, price, count);
+    cart.push(item);   
+}
+
+//function to count how many items in cart --> return total items count
+function countCart(){
+    var totalCount= 0;
+    for(var i in cart){
+        totalCount += cart[i].count;
+    }
+    return totalCount;
+}
+
+
+
+
