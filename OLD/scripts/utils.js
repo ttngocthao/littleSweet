50dff02ab72b2e67d0a0b831afea46037693e57f
ttngocthao <ttngocthao_87@yahoo.com>
2021-09-29 13:39:29 +0100

    untrack old folder

--- OLD/scripts/utils.js
+++
@@ -1,8 +0,0 @@
-/**
- * Test a value to see if it exists.
- * @param theItem 
- * @return true if the value is defined and is not null.
- */
-function isValid(theItem) {
-    return theItem != undefined && theItem != null;
-}
