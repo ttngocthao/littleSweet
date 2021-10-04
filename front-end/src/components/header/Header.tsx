c4105140eda9ee6f6ee328bf461b3fa36156d2d6
ttngocthao <ttngocthao_87@yahoo.com>
2021-10-04 09:29:05 +0100

    fixed filterbar styles,cleared unused images

--- front-end/src/components/header/Header.tsx
+++ front-end/src/components/header/Header.tsx
@@ -1,14 +1,22 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
+import { theme } from '../GlobalStyle.css';
+import {Basket3} from '@styled-icons/bootstrap/Basket3';
+
 const StyledHeader = styled.header`
     color:red;
-`
+`;
+const BasketIcon = styled(Basket3)`
+    width:30px;
+    fill: ${theme.colors.second};
+`;
+
 const Header = () => {
     return (
         <StyledHeader>
-            this is header
+            this is header <BasketIcon/>
         </StyledHeader>
-    )
-}
+    );
+};
 
-export default Header
+export default Header;
