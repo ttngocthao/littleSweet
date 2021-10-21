4af32ab6285081ca9bd8d1dc4e103f55b0c488cc
Thao Thi Ngoc Truong <ttngocthao_87@yahoo.com>
2021-10-21 12:49:28 +0100

    added basic form for auth

--- front-end/src/components/Profile/SignIn.tsx
+++ front-end/src/components/Profile/SignIn.tsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
+import styled from 'styled-components';
+
+const Field = styled.div`
+    label{
+        display: block;
+    }
+    input{
+        display: block;
+    }
+`;
 
 const SignIn = () => {
+    const [signInMode,setSignInMode]=useState(true);
+    const [inputs,setInputs] =useState({
+        email: {label:'Email',value:'',name:'email',type:'email'},
+        password:{label:'Password',value:'',name:'password',type:'password'},
+        repeatPassword:{label:'Retype Password',value:'',name:'repeatPassword',type:'password'}
+    });
+    const inputsOnChangeHandle =(e:ChangeEvent<HTMLInputElement>)=>{
+        setInputs({
+            ...inputs,
+            [e.target.name]:{...[e.target.name],value:e.target.value}
+        });
+    };
+    const displayInputs = signInMode ? Object.values(inputs) : Object.values(inputs).filter(item=>item.name!=='repeatPassword');
+    
     return (
         <div>
-            this is sign in form
+            <h1>{signInMode ? 'Sign in' : 'Sign up'}</h1>
+            <form>
+                {displayInputs.map((item)=>{
+                
+                return (<Field key={item.name}>
+                    <label>{item.label}</label>
+                    <input name={item.name} value={item.value} type={item.type} onChange={inputsOnChangeHandle}/>
+                </Field>);
+            })}
+            </form>
         </div>
     );
 };
