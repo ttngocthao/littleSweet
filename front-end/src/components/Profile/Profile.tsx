8def1255d0176b198fc0cf89ce3706594a1c853b
ttngocthao <ttngocthao_87@yahoo.com>
2021-11-01 20:39:20 +0000

    signin comp worked

--- front-end/src/components/Profile/Profile.tsx
+++ front-end/src/components/Profile/Profile.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 // import {RouteComponentProps} from '@reach/router';
-
+import { logoutAccount } from '../../utils/users';
 const Profile = () => {
     return (
         <div>
             <h1>your profile should be here</h1>
+                 <button style={{border:'1px solid red'}} onClick={logoutAccount}>log out</button>
         </div>
     );
 };
